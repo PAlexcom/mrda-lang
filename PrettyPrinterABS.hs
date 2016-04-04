4f004c4c15a9239e2d4639a4396bc5598d189dda
tommyv91 <tommaso.vuaran@gmail.com>
2016-04-04 20:29:29 +0200

    pretty printers

--- PrettyPrinterABS.hs
+++ PrettyPrinterABS.hs
@@ -2,330 +2,28 @@
 module PrettyPrinterABS where
 
 import Parser
---import Control.Monad.State
 
-
---prettyPrintABS :: AbsNode -> String
---prettyPrintABS abstractSyntaxTree = sourceCode $ execState (print_Node abstractSyntaxTree) defaultAttributes
-
---data Attributes = Attributes {
---    sourceCode :: String,
---    counterTab :: Int
---} deriving (Show)
-
---defaultAttributes = Attributes "" 0
-
---------------------------------------------------------------
------------ Enviroment Utilities -----------------------------
---------------------------------------------------------------
-
---increaseTab :: State Attributes ()
---increaseTab = do
---    modify (\attr -> attr{counterTab = (counterTab attr) + 1})
---    return ()
-
---decreaseTab :: State Attributes ()
---decreaseTab = do
---    modify (\attr -> attr{counterTab = (counterTab attr) - 1})
---    return ()
-
---appendCode :: String -> State Attributes ()
---appendCode code = do
---    modify (\attr -> attr{sourceCode = (sourceCode attr) ++ code})
---    return ()
-
---appendNewLineCode :: String -> State Attributes ()
---appendNewLineCode code = do
---    tabs <- gets counterTab
---    modify (\attr -> attr{sourceCode = (sourceCode attr) ++ newlineSymbol ++ (tabSymbol tabs) ++ code})
---    return ()
-
---prependCode :: String -> State Attributes ()
---prependCode code = do
---    modify (\attr -> attr{sourceCode = code ++ (sourceCode attr)})
---    return ()
-
---------------------------------------------------------------
--------------------------- Utilities -------------------------
---------------------------------------------------------------
-
---getIdent :: Ident -> String
---getIdent (Ident ident) = ident
-
---tabSymbol :: Int -> String
---tabSymbol n
---    | n <= 0 = ""
---    | otherwise = "    " ++ (tabSymbol (n-1))
-
---newlineSymbol :: String
---newlineSymbol = "\n"
-
---------------------------------------------------------------
------------------------- Pretty Printer ----------------------
---------------------------------------------------------------
-
---print_Program :: Program -> State Attributes ()
---print_Program (Prog decls) = do
---    print_NodeList decls
---    return ()
-
---print_Decl :: Decl -> State Attributes ()
---print_Decl node = case node of
---    DvarBInit modalityDeclNode ident basicTypeNode complexRExprNode -> do
---        appendNewLineCode $ get_Node basicTypeNode
---        appendCode " = "
---        appendCode $ ";"
---        return ()
---    DvarCInit modalityDeclNode ident typeSpecNode complexRExprNode -> do
---        return ()
---    Dfun ident parametersNode basicTypeNode compStmtNode returnStmtNode -> do
---        appendNewLineCode $ (get_Node basicTypeNode) ++ " " ++ (getIdent ident) ++ "(...) {"
---        increaseTab
---        print_Node compStmtNode
---        print_Node returnStmtNode
---        decreaseTab
---        appendNewLineCode $ "}"
---        return ()
-
---print_ReturnStmt :: ReturnStmt -> State Attributes ()
---print_ReturnStmt returnStmt = do
---    appendNewLineCode "return"
---    case returnStmt of
---        RetExpVoid -> do
---            return ()
---        RetExp rExpr -> do
---            appendCode " ("
---            print_Node rExpr
---            appendCode ")"
---            return ()
---    appendCode ";"
---    return () 
-
-
---print_CompStmt :: CompStmt -> State Attributes ()
---print_CompStmt (BlockDecl decls stmts) = do
---    print_NodeList decls
---    print_NodeList stmts
---    return ()
-
---print_Stmt :: Stmt -> State Attributes ()
---print_Stmt node = case node of
---    Comp compStmt -> do
---        print_Node compStmt
---        return ()
---    ProcCall funCall -> do
---        print_Node funCall
---        return ()
---    Jmp jumpStmt -> do
---        return ()
---    Iter iterStmt -> do
---        print_Node iterStmt
---        return ()
---    Sel selectionStmt -> do
---        print_Node selectionStmt
---        return ()
---    Assgn lExpr assignment_op rExpr -> do
---        return ()
---    LExprStmt lExpr -> do
---        print_Node lExpr
---        return ()
---    ExHandler tryCatchStmt -> do return()
-
---print_FunCall :: FunCall -> State Attributes ()
---print_FunCall (Call ident rExprs) = do
---        return () 
-
---print_IterStmt :: IterStmt -> State Attributes ()
---print_IterStmt node = case node of
---    While rExpr stmt -> do
---        appendNewLineCode "while ("
---        print_Node rExpr
---        appendCode ") {"
---        increaseTab
---        print_Node stmt
---        decreaseTab
---        appendNewLineCode "}"
---        return ()
-
---print_SelectionStmt :: SelectionStmt -> State Attributes ()
---print_SelectionStmt node = case node of
---    IfNoElse rExpr stmt -> do
---        appendNewLineCode "if ("
---        print_Node rExpr
---        appendCode ") {"
---        increaseTab
---        print_Node stmt
---        decreaseTab
---        appendCode "}"
---        return ()
---    IfElse rExpr stmt1 stmt2 -> do
---        appendNewLineCode "if ("
---        print_Node rExpr
---        appendCode ") {"
---        increaseTab
---        print_Node stmt1
---        decreaseTab
---        appendNewLineCode "} else {"
---        increaseTab
---        print_Node stmt2
---        decreaseTab
---        appendNewLineCode "}"
---        return ()
-
---print_ComplexRExpr :: ComplexRExpr -> State Attributes ()
---print_ComplexRExpr node = case node of
---    Simple rExpr -> do
---        print_Node rExpr
---        return ()
---    Array complexRExpr -> do
---        return ()
-
---print_RExpr :: RExpr -> State Attributes ()
---print_RExpr node = case node of
---    OpRelation rExpr1 rExpr2 op -> do
---        print_Node rExpr1
---        print_Node rExpr2
---        return ()
---    OpAritm rExpr1 rExpr2 op -> do
---        print_Node rExpr1
---        case op of
---            "+" -> appendCode "+"
---            "-" -> appendCode "-"
---            "*" -> appendCode "*"
---            "/" -> appendCode "/"
---            "%" -> appendCode "%"
---            "^" -> appendCode "^"
---        print_Node rExpr2
---        return ()
---    OpBoolean rExpr1 rExpr2 op -> case op of 
---        "&&" -> do
---            print_Node rExpr1
---            print_Node rExpr2
---            return ()
---        "||" -> do
---            print_Node rExpr1
---            print_Node rExpr2
---            return ()
---    Not rExpr -> do
---        print_Node rExpr
---        return ()
---    Neg rExpr -> do
---        print_Node rExpr
---        return ()
---    Ref lExpr -> do
---        return ()
---    FCall funCall -> do
---        print_Node funCall
---        return ()
---    Int integer -> do
---        appendCode $ show integer
---        return ()
---    Char char -> do
---        return ()
---    String string -> do
---        appendCode $ show string
---        return ()
---    Float float -> do
---        return ()
---    Bool boolean -> 
---        case boolean of
---            Boolean_True -> do
---                return ()
---            Boolean_False -> do
---                return ()
---    Lexpr lExpr -> do
---        print_Node lExpr
---        return ()
-
---print_LExpr :: LExpr -> State Attributes ()
---print_LExpr node = case node of
---    Deref rExpr -> do
---        return ()
---    PreIncrDecr lExpr symbol -> do
---        print_Node lExpr
---        return ()
---    PostIncrDecr lExpr symbol -> do
---        print_Node lExpr
---        return ()
---    BasLExpr bLExpr -> do
---        print_Node bLExpr
---        return () 
-
---print_BLExpr :: BLExpr -> State Attributes ()
---print_BLExpr bLExpr = case bLExpr of
---    ArrayEl bLExpr rExpr -> do 
---        return ()
---    Id ident -> do
---        appendCode $ getIdent ident
---        return ()
-
---get_BasicType :: BasicType -> String
---get_BasicType (BType basicType) = basicType
-
---------------------------------------------------------------
--------------------- Pretty Printer Node ---------------------
---------------------------------------------------------------
-
---get_Node :: AbsNode -> String
---get_Node node = case node of
---    BasicTypeNode _ node -> get_BasicType node
-
---print_Node :: AbsNode -> State Attributes ()
---print_Node node = do
---    case node of
---        RExprNode _ node         -> print_RExpr node
---        FunCallNode _ node       -> print_FunCall node
---        LExprNode _ node         -> print_LExpr node
---        BLExprNode _ node        -> print_BLExpr node
---        ProgramNode _ node       -> print_Program node
---        DeclNode _ node          -> print_Decl node
---        TypeSpecNode _ node      -> do return()
---        CompoundTypeNode _ node  -> do return()
---        ComplexRExprNode _ node  -> print_ComplexRExpr node
---        ParameterNode _ node     -> do return()
---        ModalityParamNode _ node -> do return()
---        ModalityDeclNode _ node  -> do return()
---        CompStmtNode _ node      -> print_CompStmt node
---        StmtNode _ node          -> print_Stmt node
---        Assignment_opNode _ node -> do return()
---        JumpStmtNode _ node      -> do return()
---        ReturnStmtNode _ node    -> print_ReturnStmt node
---        SelectionStmtNode _ node -> print_SelectionStmt node
---        IterStmtNode _ node      -> do return()
---        TryCatchStmtNode _ node -> do return()
---    return ()
-
---print_NodeList :: [AbsNode] -> State Attributes ()
---print_NodeList (x:xs) = do
---    print_Node x
---    print_NodeList xs
---    return ()
-
---print_NodeList [] = do
---    return ()
-
---------------- PROVA ---------------
 import Text.PrettyPrint hiding (Str)
 
-tab = 5
+tab = 1
 
 instance PrettyPrinter Program where
-    pPrt (Prog decls) = vcat . map (pPrt $ decl) decls
+    pPrt (Prog decls) = vcat ( map pPrt (map decl decls))
 
 instance PrettyPrinter Decl where
     pPrt (DvarBInit modD ident basType cRExpr) =
-        pPrt (modalityDecl modD) <+> pPrt ident <> colon <+> pPrt (basicType basType) <+> text "=" <+> pPrt (complexRExpr cRExpr)
+        pPrt (modalityDecl modD) <+> pPrt ident <> colon <+> pPrt (basicType basType) <+> text "=" <+> pPrt (complexRExpr cRExpr) <> semi
     pPrt (DvarCInit modD ident tpSpec cRExpr) =
-        pPrt (modalityDecl modD) <+> pPrt ident <> colon <+> pPrt (typeSpec tpSpec) <+> text "=" <+> pPrt (complexRExpr cRExpr)
+        pPrt (modalityDecl modD) <+> pPrt ident <> colon <+> pPrt (typeSpec tpSpec) <+> text "=" <+> pPrt (complexRExpr cRExpr) <> semi
     pPrt (Dfun ident params basType cStmt rStmt) =
-        text "def" <+> pPrt ident <+> (parens (vcat . map (pPrt $ parameter) params)) <> colon <> pPrt (basicType basType) <+> text "=" <+> braces $$ pPrt (compStmt cStmt) $$ pPrt (returnStmt rStmt)
+        text "def" <+> pPrt ident <+> (parens (hsep (punctuate comma (map pPrt (map parameter params))))) <> colon <> pPrt (basicType basType) <+> text "=" <+> lbrace $$ nest tab ((pPrt (compStmt cStmt) $+$ pPrt (returnStmt rStmt) $+$ rbrace ))
 
 instance PrettyPrinter ModalityDecl where
     pPrt ModalityD_val = text "val" 
     pPrt ModalityD_var = text "var"
 
 instance PrettyPrinter ModalityParam where
-    pPrt ModalityPEmpty = text ""
+    pPrt ModalityPEmpty = empty
     pPrt ModalityP_val = text "val"
     pPrt ModalityP_var = text "var"
     pPrt ModalityP_valres = text "valres"
@@ -342,11 +40,12 @@ instance PrettyPrinter Char where
 instance PrettyPrinter String where
     pPrt string = text string 
 
-instance PrettyPrinter Float where
-    pPrt float = text (show float) 
+instance PrettyPrinter Double where
+    pPrt double = text (show double) 
 
-instance PrettyPrinter Bool where
-    pPrt bool = text (show bool) 
+instance PrettyPrinter Boolean where
+    pPrt Boolean_True = text "true"
+    pPrt Boolean_False = text "false"
 
 instance PrettyPrinter BasicType where
     pPrt (BType tp) = text tp
@@ -361,10 +60,10 @@ instance PrettyPrinter CompoundType where
 
 instance PrettyPrinter ComplexRExpr where
     pPrt (Simple rExp) = pPrt (rExpr rExp)
-    pPrt (Array cRexprs) = text "Array" <> (parens (vcat . map (pPrt $ complexRExpr) cRexprs))
+    pPrt (Array cRexprs) = text "Array" <> (parens (hcat (punctuate comma (map pPrt (map complexRExpr cRexprs)))))
 
 instance PrettyPrinter Parameter where
-    pPrt (Param modP ident tpSpec) = pPrt (modalityParam modP) <+> pPrt ident <> colon <+> pPrt (typeSpec tpSpec)
+    pPrt (Param modP ident tpSpec) = pPrt (modalityParam modP) <+> pPrt ident <> colon <> pPrt (typeSpec tpSpec)
 
 instance PrettyPrinter RExpr where
     pPrt (OpRelation rExp1 rExp2 op) = pPrt (rExpr rExp1) <+> text op <+> pPrt(rExpr rExp2)
@@ -388,31 +87,31 @@ instance PrettyPrinter LExpr where
     pPrt (BasLExpr bLExp) = pPrt (bLExpr bLExp)
 
 instance PrettyPrinter FunCall where
-    pPrt (Call ident rExprs) = pPrt ident <> (parens (vcat . map (pPrt $ rExpr) rExprs)) 
+    pPrt (Call ident rExprs) = pPrt ident <> (parens (hcat (punctuate comma (map pPrt (map rExpr rExprs))))) 
 
 instance PrettyPrinter BLExpr where
     pPrt (ArrayEl bLExp rExp) = pPrt (bLExpr bLExp) <> (brackets (pPrt (rExpr rExp)))
     pPrt (Id ident) = pPrt ident 
 
 instance PrettyPrinter CompStmt where
-    pPrt (BlockDecl decls stmts) = (vcat . map (pPrt $ decl) decls) $$ (vcat . map (pPrt $ stmt) stmts)
+    pPrt (BlockDecl decls stmts) = (vcat (map pPrt (map decl decls))) $$ (vcat (map pPrt (map stmt stmts)))
 
 instance PrettyPrinter Stmt where
-    pPrt (Comp cStmt) = (braces ($$ pPrt (compStmt cStmt))) 
+    pPrt (Comp cStmt) = lbrace $+$ nest tab (pPrt (compStmt cStmt) $+$ rbrace ) 
     pPrt (ProcCall fcall) = pPrt (funCall fcall) <> semi
     pPrt (Jmp jStmt) = pPrt (jumpStmt jStmt) <> semi
     pPrt (Iter iStmt) = pPrt (iterStmt iStmt) 
     pPrt (Sel sStmt) = pPrt (selectionStmt sStmt)
-    pPrt (Assgn lExp op rExp) = pPrt (lExpr lExp) <+> pPrt op <+> pPrt (rExpr rExp) <> semi
+    pPrt (Assgn lExp op rExp) = pPrt (lExpr lExp) <+> pPrt (assignment_op op) <+> pPrt (rExpr rExp) <> semi
     pPrt (LExprStmt lExp) = pPrt (lExpr lExp) <> semi 
     pPrt (ExHandler tcStmt) = pPrt (tryCatch tcStmt)
 
 instance PrettyPrinter TryCatchStmt where
-    pPrt (TryCatch stm1 ident stm2) = text "try" <+> pPrt (stmt stm1) <+> text "catch" <+> (braces <+> text "case" <+> text "ex" <> colon <+> pPrt ident <+> text "=>" <+> pPrt (stmt stm2) )
+    pPrt (TryCatch stm1 ident stm2) = text "try" <+> pPrt (stmt stm1) $+$ text "catch" <+> lbrace $$ nest tab (text "case" <+> text "ex" <> colon <+> pPrt ident <+> text "=>" <+> pPrt (stmt stm2) $+$ rbrace)
 
 instance PrettyPrinter Assignment_op where
-    pPrt Assign = text "="
-    pPrt (AssignOp op) = text op <> text "="
+    pPrt Assign = equals
+    pPrt (AssignOp op) = text op <> equals
 
 instance PrettyPrinter JumpStmt where
     pPrt Break = text "break"
@@ -423,12 +122,12 @@ instance PrettyPrinter ReturnStmt where
     pPrt (RetExp rExp) = text "return" <> (parens (pPrt (rExpr rExp))) <> semi
 
 instance PrettyPrinter SelectionStmt where
-    pPrt (IfNoElse rExp stm) = text "if" <> (parens (pPrt (rExpr rExp))) <+> pPrt (stmt stm)  
+    pPrt (IfNoElse rExp stm) = text "if" <> (parens (pPrt (rExpr rExp))) <+> pPrt (stmt stm) 
     pPrt (IfElse rExp stm1 stm2) = text "if" <> (parens (pPrt (rExpr rExp))) <+> pPrt (stmt stm1) <+> text "else" <+> pPrt (stmt stm2)
 
 instance PrettyPrinter IterStmt where
     pPrt (While rExp stm) = text "while" <> (parens (pPrt (rExpr rExp))) <+> pPrt (stmt stm) 
-    pPrt (For ident rExp1 rExp2 stm) = text "for" <> (parens ((pPrt Ident) <+> text "<-" <+> (pPrt (rExpr rExp1)) <+> text "to" <+> (pPrt (rExpr rExp2)))) <+> pPrt (stmt stm)
+    pPrt (For ident rExp1 rExp2 stm) = text "for" <> (parens ((pPrt ident) <+> text "<-" <+> (pPrt (rExpr rExp1)) <+> text "to" <+> (pPrt (rExpr rExp2)))) <+> pPrt (stmt stm)
 
 class PrettyPrinter a where
   pPrt :: a -> Doc
--- PrettyPrinterTAC.hs
+++ PrettyPrinterTAC.hs
@@ -3,27 +3,6 @@
 module PrettyPrinterTAC where
 
 import CodeGenerator
-
---control :: TAC -> String
---control x = case x of
---            TACLabel label                            -> label ++ ":" ++ "\n"
---            TACBinaryOp id left op right              -> "\t" ++ id ++ " = " ++ left ++ " " ++ op ++ " " ++ right ++ "\n"
---            TACAssign id var                          -> "\t" ++ id ++ " = " ++ var ++ "\n"
---            TACUnaryOp id op var                      -> "\t" ++ id ++ " = "  ++ op ++ var ++ "\n"
---            TACCondition left op right                -> "\t" ++ left ++ " " ++ op ++ " " ++ right ++ "\n"
---            TACIf (TACCondition left op right) l1 l2  -> "\t" ++ "if " ++ left ++ op ++ right ++ " goto " ++ l1 ++ "\n" ++ "\t" ++ "goto " ++ l2 ++ "\n"
---            TACGoto label                             -> "\t" ++ "goto " ++ label ++ "\n" 
---            TACReturn label                           -> "\t" ++ "return " ++ label ++ "\n"
---            TACPreamble s                             -> "\t" ++ s ++ "\n"
---            TACParam label                            -> "\t" ++ "param " ++ label ++ "\n"
---            TACCallVoid  id npar                      -> "\t" ++ "Call " ++ id ++  "," ++ npar ++ "\n"
---            TACCall var id npar                       -> "\t" ++ var ++ " = " ++ "Call " ++ id ++  "," ++ npar ++ "\n"
---            TACException label                        -> "\t" ++ "on exception goto " ++ label ++ "\n"
-
---prettyPrintTAC :: [TAC] -> String
---prettyPrintTAC [] = ""
---prettyPrintTAC (x:xs) = (control x) ++ (prettyPrintTAC xs)
-
 import Text.PrettyPrint hiding (Str)
 
 tab = 5
